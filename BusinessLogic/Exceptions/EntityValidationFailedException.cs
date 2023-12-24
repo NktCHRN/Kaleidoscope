e347469471e11a9cf75bc0424e1a3c193bdf74e4
Nikita Chernikov <14nik20@gmail.com>
2023-12-25 00:14:19 +0200

    Implemented custom exceptions

---
+++ BusinessLogic/Exceptions/EntityValidationFailedException.cs
@@ -0,0 +1,8 @@
+﻿namespace BusinessLogic.Exceptions;
+[Serializable]
+public class EntityValidationFailedException : Exception
+{
+    public EntityValidationFailedException(string message) : base(message) { }
+
+    public EntityValidationFailedException(string message, Exception inner) : base(message, inner) { }
+}
