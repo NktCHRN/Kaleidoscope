27efad9c0386558b07c38a8f22ccca47368a8492
Nikita Chernikov <14nik20@gmail.com>
2023-12-26 19:09:19 +0200

    Added a project, added some AutoFixture customizations

---
+++ BusinessLogic.UnitTests/Customizations/RandomPostItemChildrenCustomization.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture;
+using AutoFixture.Kernel;
+using BusinessLogic.Dtos;
+using DataAccess.Entities;
+
+namespace BusinessLogic.UnitTests.Customizations;
+public class RandomPostItemChildrenCustomization : ICustomization
+{
+    public void Customize(IFixture fixture)
+    {
+        fixture.Customizations.Add(
+             new FilteringSpecimenBuilder(
+                 new RandomRelayCustomization(
+                     new TypeRelay(typeof(PostItem), typeof(ImagePostItem)),
+                     new TypeRelay(typeof(PostItem), typeof(TextPostItem))),
+                 new ExactTypeSpecification(typeof(PostItem))));
+
+        fixture.Customizations.Add(
+             new FilteringSpecimenBuilder(
+                 new RandomRelayCustomization(
+                     new TypeRelay(typeof(PostItemDto), typeof(ImagePostItemDto)),
+                     new TypeRelay(typeof(PostItemDto), typeof(TextPostItemDto))),
+                 new ExactTypeSpecification(typeof(PostItemDto))));
+    }
+}
---
+++ BusinessLogic.UnitTests/Customizations/RandomRelayCustomization.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture.Kernel;
+
+namespace BusinessLogic.UnitTests.Customizations;
+public class RandomRelayCustomization : ISpecimenBuilder
+{
+    private readonly List<ISpecimenBuilder> _builders;
+
+    public RandomRelayCustomization(params ISpecimenBuilder[] builders)
+        : this(builders.AsEnumerable())
+    {
+    }
+
+    public RandomRelayCustomization(IEnumerable<ISpecimenBuilder> builders)
+    {
+        if (builders is null)
+        {
+            throw new ArgumentNullException(nameof(builders));
+        }
+
+        _builders = builders.ToList();
+    }
+
+    public object Create(object request, ISpecimenContext context)
+    {
+        var builderIndex = Random.Shared.Next(_builders.Count);
+        var builder = _builders[builderIndex];
+        return builder.Create(request, context);
+    }
+}
