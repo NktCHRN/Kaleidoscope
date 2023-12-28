d3ae893385111c595c2d424c41e070ee383b5c68
Nikita Chernikov <14nik20@gmail.com>
2023-12-28 13:29:01 +0200

    Added second sample test class

---
+++ WebApi.IntegrationTests/Tests/BlogsControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using WebApi.IntegrationTests.Abstractions;
+using Xunit;
+
+namespace WebApi.IntegrationTests.Tests;
+public class BlogsControllerTests : ControllerTestsBase
+{
+    public BlogsControllerTests(CustomWebApplicationFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+
+    [Fact]
+    public void Test2()
+    {
+
+    }
+
+    [Fact]
+    public void Test3()
+    {
+
+    }
+}
